46e718174f2b54d5f3d4385b16dcb37528b2bd80
adel-yakoubi <adelyakoubiii@gmail.com>
2022-05-10 14:29:05 +0100

    add service to relatedlist

---
+++ lib/services/related_list.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../models/List_search.dart';
+
+class RelatedList {
+  static Future<List<Provider>> getPrestataire(String id) async {
+    final searchLists = <Provider>[];
+    var base_url = "http://102.219.178.96:3001/providers/related/";
+
+    final response = await http.get(Uri.parse('${base_url}' + id));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      for (var item in data['providers']) {
+        var provider = Provider.fromJson(item);
+        print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        provider.cover = "http://102.219.178.96:3001" + provider.cover;
+        List<String> imgs = [];
+        for (var img in provider.images) {
+          img = "http://102.219.178.96:3001" + img;
+          imgs.add(img);
+        }
+        provider.images = imgs;
+        searchLists.add(provider);
+      }
+      print(searchLists.toString() + 'zzzzzzzzzzzzzzzzzz');
+    }
+    return searchLists;
+  }
+}
